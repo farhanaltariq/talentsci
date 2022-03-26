bd51b4a7a60c6787cecd6f4855464a6d5a9d5ef5
Farhan Altariq <farhanaaxy@gmail.com>
2022-03-26 21:33:58 +0700

    set faker ready to use when database is really empty

--- application/modules/Talents/controllers/Faker.php
+++ application/modules/Talents/controllers/Faker.php
@@ -8,16 +8,46 @@ class Faker extends MX_Controller {
         $this->load->library('session');
         $this->load->model('TalentsDB');
         $this->load->model('PhotoProfileDB');
+        $this->load->database();
     }
+
     public function dataFaker(){
         include APPPATH . '/third_party/faker/src/autoload.php';
         $faker = Faker\Factory::create('id_ID');
+
+        // set default avatar
+        $this->db->where('id', 1);
+        $query = $this->db->get('photo_profile');
+        if ($query->num_rows() == 0){
+            $pict = [
+                'id' => 1,
+                'photo_profile' => 'default.webp',
+            ];
+            $this->db->insert('photo_profile', $pict);
+        }
+
+        // insert category
+        $categories = ['Chess', 'Fashion', 'Photography', 'Programming', 'Swimming'];
+        // echo "HERE"; exit();
+        for($i=1; $i<=5; $i++){
+            $this->db->where('id',$i);
+            $query = $this->db->get('category');
+            if ($query->num_rows() == 0){
+                $ctg = [
+                    'id' => $i,
+                    'name_category' => $categories[$i-1],
+                ];
+                $this->db->insert('category', $ctg);
+            }
+        }
+
+        // insert talent
         for($i=0;$i<10;$i++){
             $data['name'] = $faker->name;
             $data['email'] = $faker->email;
             $data['phone_number'] = $faker->phoneNumber;
             $data['age'] = $faker->numberBetween(18,50);
-            $data['id_photo_profile'] = $this->PhotoProfileDB->last_id() ?? 0;
+            $data['id_photo_profile'] = 1;
             $data['gender'] = $faker->randomElement(['Male','Female']);
             $data['location'] = $faker->city;
             $data['id_category'] = $faker->numberBetween(1,5);
